4e1f36bb15ea37d1f8890513131aa2e924832ff9
VictorVior <>
2022-08-25 17:40:18 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+/*Hola mundo
+Victor Vior
+25/08/2022
+Version 1*/
+//Bibliotecas
+#include <stdio.h>
+//Funcion principal
+int main(void) {
+  //Funcion de impresion en pantalla
+  printf("Hola mundo\n");
+  return 0;
+}
